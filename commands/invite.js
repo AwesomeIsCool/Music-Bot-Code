886e1e74b61d56c1a061bc6ccc56f806f42d2725
AwesomeIsCool <100255784+AwesomeIsCool@users.noreply.github.com>
2022-02-23 10:00:47 +0530

    Add files via upload

---
+++ commands/invite.js
@@ -0,0 +1,15 @@
+const i18n = require("../util/i18n");
+
+module.exports = {
+  name: "invite",
+  description: i18n.__("invite.description"),
+  execute(message) {
+    return message.member
+      .send(
+        `Made By L O R D#5705
+https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=70282305&scope=bot
+    `
+      )
+      .catch(console.error);
+  }
+};
